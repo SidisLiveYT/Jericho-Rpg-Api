427005a70f25f456703b4cc779798ebcd46970e9
Sid is Live YT <sidisliveytofficial@gmail.com>
2022-01-28 12:31:38 +0530

    removed Promises

--- database-workloads.js
+++ database-workloads.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const Mysql = require('mysql2/promise')
+const Mysql = require('mysql2')
 
 class Database {
   /**
@@ -286,7 +286,7 @@ class Database {
       valuesArray && Array.isArray(valuesArray) && valuesArray.length > 0
         ? Mysql.format(sqlString, valuesArray)
         : sqlString
-    const [rawRows] = await Database.mysqlDatabaseConnections.execute(sqlString)
+    const [rawRows] = await Database.mysqlDatabaseConnections.promise().execute(sqlString)
     return rawRows
   }
 }
